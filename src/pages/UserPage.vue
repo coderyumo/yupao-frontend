41b1cfd7171777c4e0d14471a3d63bbef34877ff
yumo <2120454642@qq.com>
2024-01-17 20:44:19 +0800

    add 补充退出功能

--- src/pages/UserPage.vue
+++ src/pages/UserPage.vue
@@ -1,21 +1,22 @@
 <template>
   <div v-if="user">
-    <van-cell title="当前用户" :value="user.userAccount"/>
-    <van-cell title="修改信息" is-link to="/user/update"/>
-    <van-cell title="我的好友" is-link to="/user/my/friend"/>
-    <van-cell title="我加入的队伍" is-link to="/team/my/join"/>
-    <van-cell title="我创建的队伍" is-link to="/team/my/create"/>
+    <van-cell :value="user.userAccount" title="当前用户"/>
+    <van-cell is-link title="修改信息" to="/user/update"/>
+    <van-cell is-link title="我的好友" to="/user/my/friend"/>
+    <van-cell is-link title="我加入的队伍" to="/team/my/join"/>
+    <van-cell is-link title="我创建的队伍" to="/team/my/create"/>
+    <van-cell is-link title="退出登录" @click="logout"/>
     <van-cell title="我的标签"/>
-    <van-row gutter="16" v-if="user.tags.length > 0" >
+    <van-row v-if="user.tags.length > 0" gutter="16">
       <van-col v-for="tag in user.tags" style="padding: 15px">
-        <van-tag type="success" size="large">{{ tag }}</van-tag>
+        <van-tag size="large" type="success">{{ tag }}</van-tag>
       </van-col>
     </van-row>
   </div>
 
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import {useRouter} from "vue-router";
 import {onMounted, ref} from "vue";
 import myAxios from "../plugins/myAxios";
@@ -26,10 +27,10 @@ const router = useRouter();
 const user = ref();
 
 onMounted(async () => {
-  const token = localStorage.getItem("token");
-  if (token == null) {
-    window.location.href='/user/login'
-  }
+      const token = localStorage.getItem("token");
+      if (token == null) {
+        window.location.href = '/user/login'
+      }
       const res = await getCurrentUser();
       if (res.code === 0) {
         if (res.data.tags) {
@@ -43,6 +44,27 @@ onMounted(async () => {
       }
     }
 )
+
+const logout = () => {
+  const token = localStorage.getItem("token");
+  console.log(token);
+  const res = myAxios.post("/user/logout", {
+    userAccount: token.split('-')[0],
+    uuid: token.split('-')[1]
+  })
+      .then(function (response) {
+        console.log('/user/logout success', response.data);
+        if (response.code === 0 && response.data) {
+          window.location.href = "/user/login"
+        }
+        return response?.data?.records;
+      })
+      .catch(function (error) {
+        console.log('/user/recommend error' + error);
+        showFailToast('请求失败');
+      });
+
+}
 
 const toEdit = (editKey: string, editName: string, currentValue: string) => {
   router.push({
